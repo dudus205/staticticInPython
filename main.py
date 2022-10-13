8745075396d49d1d30b0163978c6bbe98752fe27
Piotr Kaczmarczyk <kaczmarczyk9042@outlook.com>
2022-10-13 19:36:33 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from faker import Faker
+import pandas as pd
+
+fake = Faker(['it_IT'])
+
+
+def workers(iterations):
+    groups = [fake.color_name(), fake.color_name(), fake.color_name(), fake.color_name(), fake.color_name()]
+    full_time = round(iterations / 2)
+    half_time = round(iterations / 3)
+    part_time = round(iterations - full_time - half_time)
+    workers_dictionary = []
+
+    for i in range(iterations):
+        if full_time > 0:
+            work_type = "Full time"
+            full_time = full_time - 1
+        elif half_time > 0:
+            work_type = "Half time"
+            half_time = half_time - 1
+        elif part_time > 0:
+            work_type = "Part time"
+            part_time = part_time - 1
+
+        workers_dictionary.append([fake.random_int() * fake.random_int(1, 5),
+                                  fake.name(),
+                                  groups[fake.random_int(0, 4)],
+                                  fake.date(),  # date default is -30 years
+                                  work_type
+        ])
+
+    workers_df = pd.DataFrame(workers_dictionary)
+    return workers_df
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    company = workers(5000)
+    print(company)
